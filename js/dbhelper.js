58c3458ef10dbff3362670292bf1a0b3f41b93ab
Darryl Staflund <darryl.staflund@hushmail.com>
2019-03-16 14:11:59 -0600

    feature: Add initial service worker

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -1,4 +1,22 @@
 /**
+ * Load service worker
+ *
+ * NB:  Adaptation of code found at https://developers.google.com/web/fundamentals/primers/service-workers/
+ */
+if ('serviceWorker' in navigator){
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+        .register('/service-worker.js', { scope: '/'})
+        .then(registration => {
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, err => {
+          console.log('ServiceWorker registration failed: ', err);
+        });
+  });
+}
+
+
+/**
  * Common database helper functions.
  */
 class DBHelper {
---
+++ service-worker.js
@@ -0,0 +1,72 @@
+// Adaptation of code found at https://developers.google.com/web/fundamentals/primers/service-workers/
+const cacheName = 'assignment-1-cache-v2';
+const urlsToCache = [
+    '/',
+    '/css/restaurant-details.css',
+    '/css/restaurant-list.css',
+    '/css/styles.css',
+    '/data/restaurants.json',
+    '/img/1-750px.jpg',
+    '/img/2-750px.jpg',
+    '/img/3-750px.jpg',
+    '/img/4-750px.jpg',
+    '/img/5-750px.jpg',
+    '/img/6-750px.jpg',
+    '/img/7-750px.jpg',
+    '/img/8-750px.jpg',
+    '/img/9-750px.jpg',
+    '/img/10-750px.jpg',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/restaurant.html'
+];
+
+self.addEventListener('install', event => {
+        event.waitUntil(
+            caches.open(cacheName)
+                .then(cache => {
+                    return cache.addAll(urlsToCache);
+                })
+        );
+    }
+);
+
+self.addEventListener('activate', event => {
+        const cacheWhitelist = [cacheName];
+        event.waitUntil(
+            caches.keys().then(cacheNames => {
+                return Promise.all(
+                    cacheNames.map(cacheName => {
+                        if (cacheWhitelist.indexOf(cacheName) === -1) {
+                            return caches.delete(cacheName);
+                        }
+                    })
+                );
+            })
+        );
+    }
+);
+
+self.addEventListener('fetch', event => {
+        event.respondWith(
+            caches.match(event.request)
+                .then((response) => {
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request)
+                        .then((response) => {
+                            if (!response || response.status !== 200 || response.type !== 'basic') {
+                                return response;
+                            }
+                            const responseToCache = response.clone();
+                            caches.open(cacheName)
+                                .then((cache) => {
+                                    cache.put(event.request, responseToCache);
+                                });
+                            return response;
+                        })
+                })
+        );
+    }
+);
