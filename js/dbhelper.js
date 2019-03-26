fb94e7d7f0c3f32c6af93f5700f5a7eeae301e10
Darryl Staflund <darryl.staflund@hushmail.com>
2019-03-25 20:37:07 -0600

    fix: Add more accessibility improvements

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -155,7 +155,7 @@ class DBHelper {
     // https://leafletjs.com/reference-1.3.0.html#marker
     const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],
       {title: restaurant.name,
-      alt: restaurant.name + ' marker',
+      alt: restaurant.name + ' marker (latitude = ' + restaurant.latlng.lat + ', longitude = ' + restaurant.latlng.lng + ')',
       url: DBHelper.urlForRestaurant(restaurant)
       });
       marker.addTo(newMap);
