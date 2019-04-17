ca4a2e6ecd4aab75d775b3d5809dbb364e077492
Darryl Staflund <darryl.staflund@hushmail.com>
2019-04-17 08:05:11 -0600

    chore: Add some functions to MapService

--- scripts/service/map-service.js
+++ scripts/service/map-service.js
@@ -1,3 +1,20 @@
+const maxZoom = 18;
+const layerUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}';
+const mapToken = 'pk.eyJ1IjoiZHN0YWZsdW5kIiwiYSI6ImNqc2kzaml2cDBoM3o0NG56ZzhndmU4YjUifQ.AMO6QC2-9_sXKD4JZm3Ylg';
+
+const attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/" title="The free wiki world map">OpenStreetMap</a> contributors, ' +
+    '<a href="https://creativecommons.org/licenses/by-sa/2.0/" title="Attribution-ShareAlike 2.0 Generic license">CC-BY-SA</a>, ' +
+    'Imagery © <a href="https://www.mapbox.com/" title="An open source mapping platform for custom designed maps">Mapbox</a>';
+
+const layerOptions = {
+    mapboxToken: mapToken,
+    maxZoom: maxZoom,
+    attribution: attribution,
+    id: 'mapbox.streets'
+};
+
+
+
 class MapService {
     static _instance;
 
@@ -26,6 +43,21 @@ class MapService {
                 url: this.urlForRestaurant(restaurant)
             };
             resolve(new L.marker(coord, options));
+        });
+    }
+
+    initMap(lat, lng, zoom) {
+        return new Promise((resolve, reject) => {
+            const mapOptions = { center: [lat, lng], zoom: zoom, scrollWheelZoom: false };
+            const newMap = L.map('map', mapOptions);
+            this.addTileLayerToMap(newMap).then(() => resolve(newMap));
+        });
+    }
+
+    addTileLayerToMap(map){
+        return new Promise((resolve, reject) => {
+            L.tileLayer(layerUrl, layerOptions).addTo(map);
+            resolve();
         });
     }
 }
